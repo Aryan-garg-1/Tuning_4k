25f2e154017976d670b0c87000b676d165e2adf4
montttera <114418114+montttera@users.noreply.github.com>
2023-01-13 11:04:14 +0100

    Add files via upload

---
+++ Tuning 4k/Tuning 4k/Tuning_4kApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tuning_4kApp.swift
+//  Tuning 4k
+//
+//  Created by Mario Armento on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Tuning_4kApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tuning 4k/Tuning 4kTests/Tuning_4kTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tuning_4kTests.swift
+//  Tuning 4kTests
+//
+//  Created by Mario Armento on 13/01/23.
+//
+
+import XCTest
+@testable import Tuning_4k
+
+final class Tuning_4kTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
